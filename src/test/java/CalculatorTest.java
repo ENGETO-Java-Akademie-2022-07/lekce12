0609afd1e515177cee562c2c27d6a843eddbb709
Lukas Horak <lukas.horak@gmail.com>
2022-10-05 19:37:53 +0200

    Add java code

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+        int a = 3;
+        int b = 7;
+
+        Calculator calculator = new Calculator();
+
+        int vysledek = calculator.add(a, b);
+
+        Assertions.assertEquals(10, vysledek);
+    }
+}
